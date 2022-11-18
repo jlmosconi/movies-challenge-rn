c76d642701fb0051d9b098ff5f0f77d5b8c55f6e
jlmosconi <jlmosconi@gmail.com>
2022-11-17 21:00:44 -0300

    new icons

--- ios/anterux/AppDelegate.mm
+++ ios/anterux/AppDelegate.mm
@@ -44,7 +44,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"MoviesChallengeRN", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"Anterux", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
